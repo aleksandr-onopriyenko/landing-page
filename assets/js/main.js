0c30e1f44d8c6b018056f8926c4875a3afef7246
aleksandr-onopriyenko <new.area.for.study@gmail.com>
2022-03-07 19:26:41 +0200

    finish about-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-(function($) {})(jQuery);
+(function($) {
+  /* подключение и настрока плагина анимации */
+  AOS.init({
+    disable: 'mobile',
+    duration: 600,
+    easing: 'ease-in-sine'
+  });
+  /* подключение и настрока плагина анимации */
+})(jQuery);
