1a6501d04907cf8b942c314cab0d3ebff2920c1a
aleksandr-onopriyenko <new.area.for.study@gmail.com>
2022-03-08 05:59:28 +0200

    finish gallery-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,32 @@
 (function($) {
+  var headerNav = document.querySelector('#header-navbar') 
+  var header = document.querySelector('.header')
+  let fixedNav = () => {
+    let scroll = window.scrollY;
+    if(scroll >= 200 ){
+       headerNav.classList.add('p-my-0')  
+       header.classList.add('header-navbar--scroled')
+    } else {
+    headerNav.classList.remove('p-my-0') 
+     header.classList.remove('header-navbar--scroled');
+    }
+  }
+    window.addEventListener('scroll', fixedNav)
+
+    $(".header-navbar--scroled").hover(
+      function(){
+        $(this).parent().addClass("green")
+      },
+      function(){
+        $(this).parent().removeClass("green")
+      }
+    );
   /* подключение и настрока плагина анимации */
+  $(document).ready(function() {
+    $('.header-menu').onePageNav({
+      currentClass: 'active'
+    });
+  });
   AOS.init({
     disable: 'mobile',
     duration: 600,
