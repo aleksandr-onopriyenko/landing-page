889470104d8e3ba6546fc6e7612dec5d633cf326
aleksandr-onopriyenko <new.area.for.study@gmail.com>
2022-03-09 05:04:15 +0200

    finish footer-section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,70 @@
-(function($) {
-  var headerNav = document.querySelector('#header-navbar') 
-  var header = document.querySelector('.header')
+(function ($) {
+  var headerNav = document.querySelector("#header-navbar");
+  var header = document.querySelector(".header");
+  /* навикационное меню */
   let fixedNav = () => {
     let scroll = window.scrollY;
-    if(scroll >= 200 ){
-       headerNav.classList.add('p-my-0')  
-       header.classList.add('header-navbar--scroled')
+    if (scroll >= 200) {
+      headerNav.classList.add("p-my-0");
+      header.classList.add("header-navbar--scroled");
     } else {
-    headerNav.classList.remove('p-my-0') 
-     header.classList.remove('header-navbar--scroled');
+      headerNav.classList.remove("p-my-0");
+      header.classList.remove("header-navbar--scroled");
     }
-  }
-    fixedNav()
-    window.addEventListener('scroll', fixedNav)
+  };
+  window.addEventListener("scroll", fixedNav);
 
-    $(".header-navbar--scroled").hover(
-      function(){
-        $(this).parent().addClass("green")
-      },
-      function(){
-        $(this).parent().removeClass("green")
+  $(".header-navbar--scroled").hover(
+    function () {
+      $(this).parent().addClass("green");
+    },
+    function () {
+      $(this).parent().removeClass("green");
+    }
+  );
+  fixedNav();
+/* навикационное меню */
+/* кнопка наверх */
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() != 0) {
+        $("#backToTop").fadeIn();
+      } else {
+        $("#backToTop").fadeOut();
       }
-    );
+    });
+
+    $("#backToTop").click(function () {
+      $("body,html").animate({ scrollTop: 0 }, 800);
+    });
+  });
+/* кнопка наверх */
+/* закрытие меню при клике */
+let windowWidth = $(window).width();
+
+$(window).on('resize', () => (windowWidth = $(window).width()));
+
+let $navbarLink = $('#navbar-collapse a');
+let $navbarToggler = $('.navbar-toggler');
+let $headerNavbarCollapse = $('#navbar-collapse');
+
+$navbarLink.on('click', () => {
+  if (windowWidth < 992) {
+    $navbarToggler.addClass('collapsed');
+    $headerNavbarCollapse.removeClass('show');
+  }
+});
+/* закрытие меню при клике */
   /* подключение и настрока плагина анимации */
-  $(document).ready(function() {
-    $('.header-menu').onePageNav({
-      currentClass: 'active'
+  $(document).ready(function () {
+    $(".header-menu").onePageNav({
+      currentClass: "active",
     });
   });
   AOS.init({
-    disable: 'mobile',
+    disable: "mobile",
     duration: 600,
-    easing: 'ease-in-sine'
+    easing: "ease-in-sine",
   });
   /* подключение и настрока плагина анимации */
 })(jQuery);
